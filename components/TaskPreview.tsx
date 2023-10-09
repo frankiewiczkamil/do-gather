2830963d5b0b6d965d6abad258e65834cb85e4b9
kamil <frankiewiczkamil@gmail.com>
2023-10-09 13:24:31 +0200

    Fix task key prop :bug:

--- components/TaskPreview.tsx
+++ components/TaskPreview.tsx
@@ -1,6 +1,6 @@
-export default function TaskPreview({ name, description, status }: Task) {
+export default function TaskPreview({ name, description, status, id }: Task) {
   return (
-    <div className="flex space-x-1 border" key={name}>
+    <div className="flex space-x-1 border" key={id}>
       <div className="basis-2/12">{name}</div>
       <div className="basis-8/12 text-justify max-h-12 overflow-hidden">{description}</div>
       <div className="basis-2/12 text-center">{status}</div>
