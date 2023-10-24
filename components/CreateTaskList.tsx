acd9f1aaec0409c47ef123f5e601529b0519b302
kamil <frankiewiczkamil@gmail.com>
2023-10-24 20:06:47 +0200

    Extract title bar for modal wrapper component :recycle: :construction: :lipstick:

--- components/CreateTaskList.tsx
+++ components/CreateTaskList.tsx
@@ -1,25 +1,12 @@
 import Link from 'next/link';
-import { Modal } from '@/components/Modal';
+import { Modal } from '@/components/modal/Modal';
+import { TitleBar } from '@/components/modal/TitleBar';
 
 export function AddNew({ createTaskListAction, closePath }: { createTaskListAction: (formData: FormData) => Promise<void>; closePath: string }) {
   return (
     <Modal>
       <form action={createTaskListAction}>
-        <div className="flex items-start justify-between p-3 border-b rounded-t dark:border-gray-600 bg-gray-50">
-          <h3 className="text-xl font-semibold text-gray-900 dark:text-white">Add new list</h3>
-          <Link href={closePath}>
-            <button
-              type="button"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-hide="defaultModal"
-            >
-              <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-              </svg>
-              <span className="sr-only">Close modal</span>
-            </button>
-          </Link>
-        </div>
+        <TitleBar closePath={closePath} title="Add new task list" />
 
         <div className="flex p-4">
           <label htmlFor="new-task-list-name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
