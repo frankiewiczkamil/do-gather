0044d1405fbf20af0ed9ab897fa9e15a0d5c2277
kamil <frankiewiczkamil@gmail.com>
2023-11-16 10:52:50 +0100

    Decouple task-list reducer from its usage :truck: :recycle:

--- services/lists/infra/TaskListEventStore.ts
+++ services/lists/infra/TaskListEventStore.ts
@@ -1,74 +1,11 @@
-import { Permission, TaskList, TaskListEventFailed, TaskListIdentifier } from '@/services/lists/TaskList';
-import { CreateTaskListSucceeded } from '@/services/lists/aggregate/createTaskList';
-import { RenameTaskListSucceeded } from '@/services/lists/aggregate/renameTaskList';
-import { Task } from '@/services/lists/Task';
-import { DeleteTaskListSucceeded } from '@/services/lists/aggregate/deleteTaskList';
-import { AddTaskToListSucceeded } from '@/services/lists/aggregate/addTaskToList';
+import { TaskListEventFailed, TaskListIdentifier } from '@/services/lists/TaskList';
+import { applyTaskListEvent, createTaskListBase, isApplicableEventFactory, TaskListSuccessEvent } from '@/services/lists/infra/TaskListReducer';
 
-const taskListEventStore: Array<TaskListEventFailed | SuccessEvent> = [];
-type TaskListBase = {
-  id: TaskListIdentifier;
-  users: Permission[];
-  tasks: Task[];
-  status: 'active' | 'archived' | 'deleted';
-};
-type SuccessEvent = CreateTaskListSucceeded | RenameTaskListSucceeded | AddTaskToListSucceeded | DeleteTaskListSucceeded;
-const createTaskListBase = (taskListId: TaskListIdentifier) => ({
-  id: taskListId,
-  users: [],
-  tasks: [],
-  status: 'active' as const,
-});
+// for sake of simplicity this reducer uses a naive implementation - tasks are  array which could lead to memory issues
+// it should be implemented as some sort of stream instead in a real application
 
-export function selectTaskList(taskListId: TaskListIdentifier) {
-  return taskListEventStore
-    .filter((event) => event.status === 'succeeded' && event.taskListId === taskListId)
-    .reduce(
-      (taskList, event) => {
-        switch (event.type) {
-          case 'create-task-list-succeeded':
-            return applyCreateTaskList(taskList, event as CreateTaskListSucceeded);
-          case 'rename-task-list-succeeded':
-            return applyRenameTaskListSucceeded(taskList, event as RenameTaskListSucceeded);
-          case 'delete-task-list-succeeded':
-            return applyDeleteTaskListSucceeded(taskList, event as DeleteTaskListSucceeded);
-          case 'add-task-to-list-succeeded':
-            return applyAddTaskToListSucceeded(taskList, event as AddTaskToListSucceeded);
-          default:
-            return taskList;
-        }
-      },
-      createTaskListBase(taskListId) as TaskListBase | TaskList,
-    );
-}
-
-function applyCreateTaskList(taskListPartial: TaskListBase | TaskList, taskCreatedEvent: CreateTaskListSucceeded) {
-  return {
-    ...taskListPartial,
-    name: taskCreatedEvent.createdTaskList.name,
-    ownerId: taskCreatedEvent.authorId,
-    creatorId: taskCreatedEvent.authorId,
-    createdAt: taskCreatedEvent.timestamp,
-  };
-}
-function applyRenameTaskListSucceeded(taskList: TaskListBase | TaskList, event: RenameTaskListSucceeded) {
-  return {
-    ...taskList,
-    name: event.newName,
-  };
-}
-
-function applyDeleteTaskListSucceeded(taskList: TaskListBase | TaskList, _event: DeleteTaskListSucceeded) {
-  return {
-    ...taskList,
-    status: 'deleted' as const,
-  };
-}
-
-function applyAddTaskToListSucceeded(taskList: TaskListBase | TaskList, event: AddTaskToListSucceeded) {
-  const newTask = { id: event.task.id, name: event.task.name, status: 'new' as const, description: event.task.description };
-  return {
-    ...taskList,
-    tasks: [...taskList.tasks, newTask],
-  };
+// this is a naive implementation of a reducer, as it uses an array of events instead of a stream (for sake of simplicity)
+// it should be implemented as some sort of stream instead in a real application
+export function selectTaskList(taskList: Array<TaskListEventFailed | TaskListSuccessEvent>, taskListId: TaskListIdentifier) {
+  return taskList.filter(isApplicableEventFactory(taskListId)).reduce(applyTaskListEvent, createTaskListBase(taskListId));
 }
---
+++ services/lists/infra/TaskListReducer.ts
@@ -0,0 +1,66 @@
+import { Permission, TaskList, TaskListEvent, TaskListIdentifier } from '@/services/lists/TaskList';
+import { CreateTaskListSucceeded } from '@/services/lists/aggregate/createTaskList';
+import { RenameTaskListSucceeded } from '@/services/lists/aggregate/renameTaskList';
+import { DeleteTaskListSucceeded } from '@/services/lists/aggregate/deleteTaskList';
+import { AddTaskToListSucceeded } from '@/services/lists/aggregate/addTaskToList';
+import { Task } from '@/services/lists/Task';
+
+export function applyTaskListEvent(taskList: TaskList | TaskListBase, event: TaskListEvent | TaskListSuccessEvent) {
+  switch (event.type) {
+    case 'create-task-list-succeeded':
+      return applyCreateTaskList(taskList, event as CreateTaskListSucceeded);
+    case 'rename-task-list-succeeded':
+      return applyRenameTaskListSucceeded(taskList, event as RenameTaskListSucceeded);
+    case 'delete-task-list-succeeded':
+      return applyDeleteTaskListSucceeded(taskList, event as DeleteTaskListSucceeded);
+    case 'add-task-to-list-succeeded':
+      return applyAddTaskToListSucceeded(taskList, event as AddTaskToListSucceeded);
+    default:
+      return taskList;
+  }
+}
+export const isApplicableEventFactory = (taskListId: TaskListIdentifier) => (event: TaskListEvent | TaskListSuccessEvent) =>
+  event.taskListId === taskListId && event.status === 'succeeded';
+
+function applyCreateTaskList(taskListPartial: TaskListBase | TaskList, taskCreatedEvent: CreateTaskListSucceeded) {
+  return {
+    ...taskListPartial,
+    name: taskCreatedEvent.createdTaskList.name,
+    ownerId: taskCreatedEvent.authorId,
+    creatorId: taskCreatedEvent.authorId,
+    createdAt: taskCreatedEvent.timestamp,
+  };
+}
+function applyRenameTaskListSucceeded(taskList: TaskListBase | TaskList, event: RenameTaskListSucceeded) {
+  return {
+    ...taskList,
+    name: event.newName,
+  };
+}
+
+function applyDeleteTaskListSucceeded(taskList: TaskListBase | TaskList, _event: DeleteTaskListSucceeded) {
+  return {
+    ...taskList,
+    status: 'deleted' as const,
+  };
+}
+
+function applyAddTaskToListSucceeded(taskList: TaskListBase | TaskList, event: AddTaskToListSucceeded) {
+  const newTask = { id: event.task.id, name: event.task.name, status: 'new' as const, description: event.task.description };
+  return {
+    ...taskList,
+    tasks: [...taskList.tasks, newTask],
+  };
+}
+
+type TaskListBase = {
+  id: TaskListIdentifier;
+  users: Permission[];
+  tasks: Task[];
+  status: 'active' | 'archived' | 'deleted';
+};
+export type TaskListSuccessEvent = CreateTaskListSucceeded | RenameTaskListSucceeded | AddTaskToListSucceeded | DeleteTaskListSucceeded;
+
+export function createTaskListBase(taskListId: TaskListIdentifier) {
+  return { id: taskListId, users: [], tasks: [], status: 'active' as const };
+}
